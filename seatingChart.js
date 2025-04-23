cd637f11e44110b8ab1adad2da0c93c98d5964a2
dnoce3 <dnoce3@gatech.edu>
2025-04-23 11:53:33 -0400

    create repo

---
+++ seatingChart.js
@@ -0,0 +1,382 @@
+
+let seatingChart = new Map();
+seatingChart.set("Derek Noce", [1]);
+seatingChart.set("Kimberly Noce", [1]);
+seatingChart.set("Holly Noce", [1]);
+seatingChart.set("Luke Noce", [1]);
+seatingChart.set("Alphonse Bertoni", [1]);
+seatingChart.set("Denise Bertoni", [1]);
+seatingChart.set("Nick Bertoni", [1]);
+seatingChart.set("Brandi Bertoni", [1]);
+seatingChart.set("Nicholas Bertoni", [1]);
+seatingChart.set("William Bertoni", [1]);
+seatingChart.set("Jaime Lieber", [2]);
+seatingChart.set("Hal Lieber", [2]);
+seatingChart.set("Elianna Lieber", [2]);
+seatingChart.set("Stuart Lieber", [2]);
+seatingChart.set("Larry Bell", [2]);
+seatingChart.set("Lydia Bell", [2]);
+seatingChart.set("Jim Kaley", [2]);
+seatingChart.set("Janet Kaley", [2]);
+seatingChart.set("Steve Kreyling", [2]);
+seatingChart.set("Kathy Kreyling", [2]);
+seatingChart.set("James Canzoneri", [3]);
+seatingChart.set("Kathleen Canzoneri", [3]);
+seatingChart.set("James Canzoneri Jr.", [3]);
+seatingChart.set("Judson Canzoneri", [3]);
+seatingChart.set("Joseph Canzoneri", [3]);
+seatingChart.set("Sandra Oswald", [3]);
+seatingChart.set("Arthur Oswald", [3]);
+seatingChart.set("Jason Oswald", [3]);
+seatingChart.set("Sue Hall", [3]);
+seatingChart.set("Nick Hall", [3]);
+seatingChart.set("Phillip Goldsmith", [4]);
+seatingChart.set("Stephanie Goldsmith", [4]);
+seatingChart.set("Nathan Goldsmith", [4]);
+seatingChart.set("Young Che", [4]);
+seatingChart.set("Jane Che", [4]);
+seatingChart.set("Sarah Che", [4]);
+seatingChart.set("David Che", [4]);
+seatingChart.set("Sanjay Kumar", [4]);
+seatingChart.set("Sanjay's Guest", [4]);
+seatingChart.set("Glen Magloff", [5]);
+seatingChart.set("Cindy Magloff", [5]);
+seatingChart.set("Tobin Bossola", [5]);
+seatingChart.set("Rachel Bossola", [5]);
+seatingChart.set("Tom Mandt", [5]);
+seatingChart.set("Holly Mandt", [5]);
+seatingChart.set("Bill Lucier", [5]);
+seatingChart.set("Erica Lucier", [5]);
+seatingChart.set("Rick Fowler", [5]);
+seatingChart.set("Christine Fowler", [5]);
+seatingChart.set("Frank DeProspo", [6]);
+seatingChart.set("Nicole DeProspo", [6]);
+seatingChart.set("Russ Tooke", [6]);
+seatingChart.set("Caren Tooke", [6]);
+seatingChart.set("Adam Williby", [6]);
+seatingChart.set("Bree Williby", [6]);
+seatingChart.set("Zeanna Graves", [6]);
+seatingChart.set("Betty Graves", [6]);
+seatingChart.set("Ron Graves", [6]);
+seatingChart.set("Zack Graves", [6]);
+seatingChart.set("Le Tran", [7]);
+seatingChart.set("Le's Guest", [7]);
+seatingChart.set("Joe D'Ambrosio", [7]);
+seatingChart.set("Joe's Guest", [7]);
+seatingChart.set("Anita Azam", [7]);
+seatingChart.set("Anita's Guest", [7]);
+seatingChart.set("Warren Tyre", [7]);
+seatingChart.set("Mary Margaret Tyre", [7]);
+seatingChart.set("Laurie O'Hara", [7]);
+seatingChart.set("Nan O'Hara", [7]);
+seatingChart.set("Lauren Sonnier", [8]);
+seatingChart.set("Rowan Muhl", [8]);
+seatingChart.set("Ashley Werner", [8]);
+seatingChart.set("Melissa Werner", [8]);
+seatingChart.set("Nick Califano", [9]);
+seatingChart.set("Morgan Califano", [9]);
+seatingChart.set("Phil Long", [9]);
+seatingChart.set("Madeline Lewis", [9]);
+seatingChart.set("Lucas Califano", [9]);
+seatingChart.set("Abigail Yazdia", [9]);
+seatingChart.set("Samuel Califano", [9]);
+seatingChart.set("Isabela Califano", [9]);
+seatingChart.set("Sophia Califano", [9]);
+seatingChart.set("Clay Carter", [10]);
+seatingChart.set("Presley Carter", [10]);
+seatingChart.set("Nelson Norris", [10]);
+seatingChart.set("Sabastian Fallon", [10]);
+seatingChart.set("Adam Demeter", [10]);
+seatingChart.set("Alison Haas", [10]);
+seatingChart.set("Ayden Rebhan", [10]);
+seatingChart.set("Cassidy Roberts", [10]);
+seatingChart.set("Larry Langtios", [10]);
+seatingChart.set("Quint Johnson", [10]);
+seatingChart.set("Jennifer Chudgar", [11]);
+seatingChart.set("Virginia Woerishoffer", [11]);
+seatingChart.set("Mark Woerishoffer", [11]);
+seatingChart.set("Carolyn Grams", [11]);
+seatingChart.set("Robert Grams", [11]);
+seatingChart.set("Norman Grams", [11]);
+seatingChart.set("Julie Grams", [11]);
+seatingChart.set("Jeremiah King", [11]);
+seatingChart.set("Stacy King", [11]);
+seatingChart.set("Everly King", [11]);
+seatingChart.set("Eric Grams", [12]);
+seatingChart.set("Melissa Grams", [12]);
+seatingChart.set("Harper Grams", [12]);
+seatingChart.set("Logan Grams", [12]);
+seatingChart.set("Makenna Grams", [12]);
+seatingChart.set("Charles Schied", [12]);
+seatingChart.set("MaryGail Schied", [12]);
+seatingChart.set("Becky Grams", [12]);
+seatingChart.set("Nick Grams", [12]);
+seatingChart.set("Gene Ruckh", [13]);
+seatingChart.set("Bonnie Ruckh", [13]);
+seatingChart.set("Brandon Ruckh", [13]);
+seatingChart.set("Max Ruckh", [13]);
+seatingChart.set("Danielle Ruckh", [13]);
+seatingChart.set("Aaron Ruckh", [13]);
+seatingChart.set("Jim Jenkins", [13]);
+seatingChart.set("Kristi Jenkins", [13]);
+seatingChart.set("Kaden Jenkins", [13]);
+seatingChart.set("Sam Jenkins", [13]);
+seatingChart.set("Crawford Strunk", [14]);
+seatingChart.set("Shelly Strunk", [14]);
+seatingChart.set("Asa Strunk", [14]);
+seatingChart.set("Everett Strunk", [14]);
+seatingChart.set("Emma Strunk", [14]);
+seatingChart.set("Sydney Baldwin", [14]);
+seatingChart.set("Sydney's Guest", [14]);
+seatingChart.set("Derek Cunningham", [15]);
+seatingChart.set("Dawn Cunningham", [15]);
+seatingChart.set("Mark Stoddart", [15]);
+seatingChart.set("Jennifer Stodart", [15]);
+seatingChart.set("Karl Zacek", [15]);
+seatingChart.set("Vera Zacek", [15]);
+seatingChart.set("Jigar Shah", [16]);
+seatingChart.set("Dipali Shah", [16]);
+seatingChart.set("Aadi Shah", [16]);
+seatingChart.set("Caitlin Kenney", [16]);
+seatingChart.set("Pratima Shah", [16]);
+seatingChart.set("Dharendra Shah", [16]);
+seatingChart.set("Chintu Shah", [16]);
+seatingChart.set("Prima Shah", [16]);
+seatingChart.set("Jainim Shah", [16]);
+seatingChart.set("Nandani Patel", [16]);
+seatingChart.set("Siren Chudgar", [18]);
+seatingChart.set("Rachana Zaveri", [18]);
+seatingChart.set("Minesh Zaveri", [18]);
+seatingChart.set("Riya Zaveri", [18]);
+seatingChart.set("Karina Zaveri", [18]);
+seatingChart.set("Dylan Zaveri", [18]);
+seatingChart.set("Jatin Chudgar", [18]);
+seatingChart.set("Hemisha Chudgar", [18]);
+seatingChart.set("Mayak Shah", [19]);
+seatingChart.set("Smita Shah", [19]);
+seatingChart.set("Atit Shah", [19]);
+seatingChart.set("Lina Shah", [19]);
+seatingChart.set("Aria Shah", [19]);
+seatingChart.set("Ava Shah", [19]);
+seatingChart.set("Amol Shah", [19]);
+seatingChart.set("Jenita Shah", [19]);
+seatingChart.set("Maya Shah", [19]);
+seatingChart.set("Mansi Chudgar", [20]);
+seatingChart.set("Kajol Chudgar", [20]);
+seatingChart.set("Mohit Pandya", [20]);
+seatingChart.set("Arti Shah", [20]);
+seatingChart.set("Akash Shah", [20]);
+seatingChart.set("Pinky Shah", [20]);
+seatingChart.set("Sonali Shah", [20]);
+seatingChart.set("Mohit Jani", [20]);
+seatingChart.set("Nil Shah", [20]);
+seatingChart.set("Shaan Chudgar", [18,20]);
+
+
+let allNames = ["Aadi Shah", "Aaron Ruckh", "Abigail Yazdia", "Adam Demeter", "Adam Williby", "Akash Shah", "Alison Haas", "Alphonse Bertoni", "Amol Shah", "Anita Azam", "Anita's Guest", "Aria Shah", "Arthur Oswald", "Arti Shah", "Asa Strunk", "Ashley Werner", "Atit Shah", "Ava Shah", "Ayden Rebhan", "Becky Grams", "Betty Graves", "Bill Lucier", "Bonnie Ruckh", "Brandi Bertoni", "Brandon Ruckh", "Bree Williby", "Caitlin Kenney", "Caren Tooke", "Carolyn Grams", "Cassidy Roberts", "Charles Schied", "Chintu Shah", "Christine Fowler", "Cindy Magloff", "Clay Carter", "Crawford Strunk", "Danielle Ruckh", "David Che", "Dawn Cunningham", "Denise Bertoni", "Derek Cunningham", "Derek Noce", "Dharendra Shah", "Dipali Shah", "Dylan Zaveri", "Elianna Lieber", "Emma Strunk", "Eric Grams", "Erica Lucier", "Everett Strunk", "Everly King", "Frank DeProspo", "Gene Ruckh", "Glen Magloff", "Hal Lieber", "Harper Grams", "Hemisha Chudgar", "Holly Mandt", "Holly Noce", "Isabela Califano", "Jaime Lieber", "Jainim Shah", "James Canzoneri", "James Canzoneri Jr.", "Jane Che", "Janet Kaley", "Jason Oswald", "Jatin Chudgar", "Jenita Shah", "Jennifer Chudgar", "Jennifer Stodart", "Jeremiah King", "Jigar Shah", "Jim Jenkins", "Jim Kaley", "Joe D'Ambrosio", "Joe's Guest", "Joseph Canzoneri", "Judson Canzoneri", "Julie Grams", "Kaden Jenkins", "Kajol Chudgar", "Karina Zaveri", "Karl Zacek", "Kathleen Canzoneri", "Kathy Kreyling", "Kimberly Noce", "Kristi Jenkins", "Larry Bell", "Larry Langtios", "Lauren Sonnier", "Laurie O'Hara", "Le Tran", "Le's Guest", "Lina Shah", "Logan Grams", "Lucas Califano", "Luke Noce", "Lydia Bell", "Madeline Lewis", "Makenna Grams", "Mansi Chudgar", "Mark Stoddart", "Mark Woerishoffer", "Mary Margaret Tyre", "MaryGail Schied", "Max Ruckh", "Maya Shah", "Mayak Shah", "Melissa Grams", "Melissa Werner", "Minesh Zaveri", "Mohit Jani", "Mohit Pandya", "Morgan Califano", "Nan O'Hara", "Nandani Patel", "Nathan Goldsmith", "Nelson Norris", "Nicholas Bertoni", "Nick Bertoni", "Nick Califano", "Nick Grams", "Nick Hall", "Nicole DeProspo", "Nil Shah", "Norman Grams", "Phil Long", "Phillip Goldsmith", "Pinky Shah", "Pratima Shah", "Presley Carter", "Prima Shah", "Quint Johnson", "Rachana Zaveri", "Rachel Bossola", "Rick Fowler", "Riya Zaveri", "Robert Grams", "Ron Graves", "Rowan Muhl", "Russ Tooke", "Sabastian Fallon", "Sam Jenkins", "Samuel Califano", "Sandra Oswald", "Sanjay Kumar", "Sanjay's Guest", "Sarah Che", "Shaan Chudgar", "Shelly Strunk", "Siren Chudgar", "Smita Shah", "Sonali Shah", "Sophia Califano", "Stacy King", "Stephanie Goldsmith", "Steve Kreyling", "Stuart Lieber", "Sue Hall", "Sydney Baldwin", "Sydney's Guest", "Tobin Bossola", "Tom Mandt", "Vera Zacek", "Virginia Woerishoffer", "Warren Tyre", "William Bertoni", "Young Che", "Zack Graves", "Zeanna Graves", "nan", ]
+
+
+
+// =================== CALLBACKS =========================
+
+function clearInfo() {
+
+  document.getElementById('tableNum0').style.display = "none";
+  document.getElementById('line0').style.display = "none";
+  var guestList = document.getElementById('tableGuests0');
+  while (guestList.firstChild) {
+    guestList.removeChild(guestList.firstChild);
+  }
+
+  document.getElementById('tableNum1').style.display = "none";
+  document.getElementById('line1').style.display = "none";
+  guestList = document.getElementById('tableGuests1');
+  while (guestList.firstChild) {
+    guestList.removeChild(guestList.firstChild);
+  }
+
+  document.getElementById('errorMsg').style.display = "none";
+
+}
+
+function searchChart() {
+
+  let name = document.getElementById('name').value;
+
+  clearInfo();
+
+  // Checking if a key exists
+  if (seatingChart.has(name)) {
+
+    // Clear error message
+    document.getElementById('errorMsg').style.display = "none";
+
+    // Populate table number
+    var table = seatingChart.get(name);
+    
+    if (table.length > 1) {
+      var msg = document.getElementById('errorMsg');
+      msg.textContent = "Multiple guests are named " + name + ". " + "Please sit with your party.";
+      msg.style.display = "block";
+    }
+
+    for (let t in table) {
+      console.log(table[t]);
+      document.getElementById('tableNum' + t).innerText = "TABLE " + table[t];
+      document.getElementById('tableNum' + t).style.display = "block";
+      document.getElementById('line' + t).style.display = "block";
+
+      // Get all guests at table
+
+      var guestList = document.getElementById('tableGuests' + t);
+      for (const [guest, tableNum] of seatingChart.entries()) {
+        if (tableNum.includes(table[t])) {
+          var guestItem = document.createElement('li');
+          var guestName = document.createElement('p');
+          guestName.textContent = guest;
+          guestName.setAttribute('class', 'regText');
+          guestItem.appendChild(guestName);
+
+          guestList.appendChild(guestItem);
+
+          // tableMembers.push(guest);
+        }
+      }    
+    }
+    
+  }
+  else {
+    clearInfo();
+
+    // Display error message
+    if (name != "") {
+      var msg = document.getElementById('errorMsg');
+      msg.textContent = "'" + name + "'" + " was not found. Check capitalization";
+      msg.style.display = "block";
+    }
+    
+  }
+}
+
+
+// ====================== BUTTONS ==========================
+const enterButton = document.querySelector(".enterButton");
+enterButton.addEventListener("click", searchChart);
+
+const searchBar = document.getElementById("name");
+searchBar.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        searchChart();
+    }
+    else {
+      clearInfo();
+    }
+});
+
+autocomplete(document.getElementById("name"), allNames);
+
+let autompletedName = "";
+
+
+// ================= AUTOCOMPLETE ========================
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          // b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          b.setAttribute('data-value', arr[i]);
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              // inp.value = this.getElementsByTagName("input")[0].value;
+              inp.value = this.getAttribute('data-value');
+              // SUBMIT THE SEARCH
+              searchChart();
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+}
